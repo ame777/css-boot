73402eca300f0a71f5e7174b8179504f46604fbc
Amadou ndiaye <amandiaye777@gmail.com>
2024-02-14 12:15:59 +0000

    initial commit

---
+++ src/test/java/cssboot/CssBootApplicationTests.java
@@ -0,0 +1,13 @@
+package cssboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CssBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
